da448b6916f6755d729ce7198f6094d003d02faf
Juank <juankrloslabre123@gmail.com>
2021-03-18 16:15:54 -0500

    Restar

--- src/app/operaciones/operaciones.component.ts
+++ src/app/operaciones/operaciones.component.ts
@@ -66,7 +66,81 @@ export class OperacionesComponent implements OnInit {
   }
 
   Restar(){
-
+    this.resultado = this.restarInterno(this.valor1, this.valor2)
   }
 
+  restarInterno(num1:string, num2:string):string
+  {
+      if(num1==num2)
+      {
+          return "0";
+      }
+      else if (Number(num1) > Number(num2)) {
+      return this.restarOrdenado(num1, num2);
+    } else {
+      return this.restarOrdenado(num2, num1)
+    }
+  }
+
+  restarOrdenado(num1:any, num2:any):string
+  {
+    var num1_idx = num1.length - 1;
+    var num2_idx = num1.length - num2.length;
+    var remainder = 0;
+    var respuesta = new Array();
+
+      for (let index = num1_idx; index >= 0; index--)
+      {
+          if(index-num2_idx>= 0)
+          {
+
+              var sumaRemainder = Number(num2[index-num2_idx]) + remainder;
+              if(num1[index] >= sumaRemainder)
+              {
+                  var res = num1[index] - sumaRemainder;
+                  respuesta.push(res);
+                  remainder = 0;
+              }
+              else
+              {
+                  var suma = Number(num1[index]) + 10;
+                  var res = suma - sumaRemainder;
+                  respuesta.push(res);
+                  remainder = 1;
+              }
+          }
+          else
+          {
+              if(remainder > 0)
+              {
+                  if(Number(num1[index])>=remainder)
+                  {
+                      var res = num1[index] - remainder;
+                      respuesta.push(res);
+                      remainder = 0;
+                  }
+                  else
+                  {
+                      var suma = Number(num1[index]) + 10;
+                      var res = suma - remainder;
+                      respuesta.push(res);
+                      remainder = 1;
+                  }
+              }
+              else
+              {
+                  var res = num1[index] - remainder;
+                      respuesta.push(res);
+                      remainder = 0;
+              }
+          }
+
+      }
+      var respuestaOrdenada = "";
+      for (let index = respuesta.length-1; index >=0; index--)
+      {
+          respuestaOrdenada += respuesta[index];
+      }
+      return respuestaOrdenada;
+  }
 }
